9a381d8037bc0e97ce50725d9b00bf1008941a1f
Manuel Schneider <manuelschneid3r@gmail.com>
2023-12-20 14:57:15 +0100

    [python:4.0]
    * Fix pip install commandline
    * iid:0.23
    * Drop source watches. a plugin provider cant just reload without notifying the plugin registry
    * Print more info on NoPluginException
    * API 2.2
      - PluginInstance.configWidget supports 'label'
      - __doc__ is not used anymore, since 0.23 drops long_description metadata
      - md_maintainers not used anymore
      - md_authors new optional field
    * i18n
    * german translation

--- python/src/plugin.h
+++ python/src/plugin.h
@@ -3,32 +3,24 @@
 #pragma once
 #include "albert/extension/pluginprovider/pluginprovider.h"
 #include "albert/plugin.h"
-#include <QFileSystemWatcher>
 #include <memory>
 class PyPluginLoader;
 namespace pybind11 { class gil_scoped_release; }
 
-class Plugin : public albert::plugin::ExtensionPlugin<albert::PluginProvider>
+class Plugin : public albert::plugin::ExtensionPlugin,
+               public albert::PluginProvider
 {
     Q_OBJECT ALBERT_PLUGIN
 public:
     Plugin();
     ~Plugin() override;
 
-    bool watchSources() const;
-    void setWatchSources(bool);
-
-protected:
     QWidget* buildConfigWidget() override;
     std::vector<albert::PluginLoader*> plugins() override;
 
 private:
-    void reloadModules();
-    void updateSourceWatches();
-
     std::vector<std::unique_ptr<PyPluginLoader>> plugins_;
     std::unique_ptr<pybind11::gil_scoped_release> release_;
-    std::unique_ptr<QFileSystemWatcher> sources_watcher_;
 };
 
 
